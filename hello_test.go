6ba1bba0edd666005fa1b0b2ee51b71dd57b7a8d
Devatanu Banerjee <devatanu@gmail.com>
2024-12-17 19:32:50 -0500

    chore: closing previous develop branch before pulling from main

--- hello_test.go
+++ hello_test.go
@@ -1,12 +1,12 @@
-package main
+// package main
 
-import "testing"
+// import "testing"
 
-func TestGetGreeting(t *testing.T) {
-	got := getGreeting()
-	want := "Hello world"
+// func TestGetGreeting(t *testing.T) {
+// 	got := getGreeting()
+// 	want := "Hello world"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
-}
+// 	if got != want {
+// 		t.Errorf("got %q, want %q", got, want)
+// 	}
+// }
